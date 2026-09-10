433cf43c3ceacfe5e84c4f8498095057d73945bc
Juho Härme <juho.harme@gmail.com>
2026-09-10 21:45:07 +0300

    add base path as var

--- apps/telifront/src/main.tsx
+++ apps/telifront/src/main.tsx
@@ -8,7 +8,7 @@ import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, basepath: import.meta.env.BASE_URL });
 const queryClient = new QueryClient();
 
 // Register the router instance for type safety
--- apps/telifront/vite.config.js
+++ apps/telifront/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [
     tanstackRouter({
       target: "react",
